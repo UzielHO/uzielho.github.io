be5235e600e643f8b53d1e8fd6097810042cec85
Uziel HO <uzielihu_@live.com.mx>
2025-06-21 21:34:20 -0600

    Updates

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'http://uzielho.github.io/uzielho.github.io',
+  base: '/secta-banditos-app/',
 });
