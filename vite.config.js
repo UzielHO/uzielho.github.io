759efde263cfe668478cb2a85dfcba7ca056d418
Uziel HO <uzielihu_@live.com.mx>
2025-06-21 21:46:48 -0600

    added url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/uzielho.github.io/',
+  base: 'http://uzielho.github.io/uzielho.github.io',
 });
